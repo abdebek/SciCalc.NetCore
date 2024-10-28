56421ebf873b4dacd7a5c1baf6e03ff4463edabf
Abdullah D. <abdeadekebo@gmail.com>
2024-10-28 11:41:55 +0300

    chore: Simplify array initialization in CalculatorTests.cs
    The initialization of the `testCases` array has been simplified.
    Instead of using the `new[]` syntax to create the array, the code
    now directly uses square brackets `[]` to initialize the array.
    This change does not affect the functionality but makes the code
    slightly more concise.

--- SciCalcDemo/SciCalcTests/CalculatorTests.cs
+++ SciCalcDemo/SciCalcTests/CalculatorTests.cs
@@ -4,15 +4,15 @@ namespace SciCalcDemo.SciCalcTests;
 
 public static class CalculatorTests
 {
-    private static readonly (string, string)[] testCases = new[]
-    {
+    private static readonly (string, string)[] testCases =
+    [
         ("1.23", "4.56"),        // Same decimal places
         ("12.1034", "5.67"),     // Different decimal places
         ("1.234e3", "5.67"),     // Scientific notation
         ("1200", "1.23"),        // Trailing zeros
         ("0.00120", "1.23"),     // Leading zeros
         ("0.00120", "0.000123"), // Very small numbers
-    };
+    ];
 
     public static void RunAllTests()
     {
